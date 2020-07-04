f2568ce664ca0efa7fa8c9a156367b94e127e402
Virjanand <virjanand@virjanand.com>
2020-07-04 14:30:16 +0200

    refactor to BDD tests

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitSDJpaServiceTest.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.repositories.VisitRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +14,8 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,54 +27,70 @@ class VisitSDJpaServiceTest {
     @InjectMocks
     private VisitSDJpaService visitSDJpaService;
 
+    @DisplayName("Test find all")
     @Test
     void findAll() {
+        //given
         Visit visit = new Visit();
-
         Set<Visit> visits = new HashSet<>();
         visits.add(visit);
-        when(visitRepository.findAll()).thenReturn(visits);
+        given(visitRepository.findAll()).willReturn(visits);
 
+        //when
         Set<Visit> foundVisits = visitSDJpaService.findAll();
 
-        verify(visitRepository).findAll();
-
+        //then
+        then(visitRepository).should().findAll();
         assertThat(foundVisits).isEqualTo(visits);
     }
 
     @Test
     void findById() {
+        //given
         Visit expectedVisit = new Visit();
-        when(visitRepository.findById(anyLong())).thenReturn(java.util.Optional.of(expectedVisit));
+        given(visitRepository.findById(anyLong())).willReturn(java.util.Optional.of(expectedVisit));
 
-        assertThat(visitSDJpaService.findById(1L)).isEqualTo(expectedVisit);
-        verify(visitRepository).findById(anyLong());
+        //when
+        Visit foundVisit = visitSDJpaService.findById(1L);
+
+        //then
+        assertThat(foundVisit).isEqualTo(expectedVisit);
+        then(visitRepository).should().findById(anyLong());
     }
 
     @Test
     void save() {
+        //given
         Visit visit = new Visit();
-        when(visitRepository.save(any(Visit.class))).thenReturn(visit);
+        given(visitRepository.save(any(Visit.class))).willReturn(visit);
 
-        assertThat(visitSDJpaService.save(visit)).isEqualTo(visit);
+        //when
+        Visit savedVisit = visitSDJpaService.save(visit);
 
-        verify(visitRepository).save(visit);
+        //then
+        assertThat(savedVisit).isEqualTo(visit);
+        then(visitRepository).should().save(visit);
     }
 
     @Test
     void delete() {
+        //given
         Visit visit = new Visit();
 
+        //when
         visitSDJpaService.delete(visit);
 
-        verify(visitRepository).delete(visit);
+        //then
+        then(visitRepository).should().delete(visit);
     }
 
     @Test
     void deleteById() {
 
+        //when
         visitSDJpaService.deleteById(1L);
 
-        verify(visitRepository).deleteById(1L);
+        //then
+        then(visitRepository).should().deleteById(1L);
     }
 }
